cd29f5bdad2821e280f7b448c5ffa3ae91893c42
Nadietepregunto <89047416+Lau-Gonz@users.noreply.github.com>
2022-08-27 10:11:08 -0500

    404

--- TCPcliente.py
+++ TCPcliente.py
@@ -3,9 +3,12 @@ from socket import *
 servidorNombre = "127.0.0.1" 
 servidorPuerto = 12000
 clienteSocket = socket(AF_INET, SOCK_STREAM)
-clienteSocket.connect((servidorNombre,servidorPuerto))
-mensaje = input("Ingrese un mensaje:")
-clienteSocket.send(bytes(mensaje, "utf-8"))
-mensajeRespuesta = clienteSocket.recv(1024)
-print("Respuesta:\n" + str(mensajeRespuesta, "utf-8"))
-clienteSocket.close()
+try:
+    clienteSocket.connect((servidorNombre,servidorPuerto))
+    mensaje = input("Ingrese un mensaje:")
+    clienteSocket.send(bytes(mensaje, "utf-8"))
+    mensajeRespuesta = clienteSocket.recv(1024)
+    print("Respuesta:\n" + str(mensajeRespuesta, "utf-8"))
+    clienteSocket.close()
+except ConnectionRefusedError:
+    print("404: Server not found")
